7a133e6f702ca05fd41e0ad207cb7f5eb53ba146
c0ff33b34n <86168298+c0ff33b34n@users.noreply.github.com>
2021-10-02 22:03:32 +0530

    Add files via upload

---
+++ subTakeOver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#c0ff33b34n
+
+echo "Target:"
+read tar
+
+if [ -f "domains" ] ; then
+    rm domains
+fi
+
+
+subfinder -d $tar -all -o domains ;
+assetfinder -subs-only $tar >> domains ;
+amass enum -norecursive -noalts -d $tar >> domains ;
+
+# add more subFinding methods for better results.
+
+cat domains|grep $tar|sort -u > subdata.txt;
+subjack -w subdata.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 >> takeOver.txt ;
